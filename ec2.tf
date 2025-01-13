279cf83f3736a600e859ebcbf2b3c697036f0b74
Kushagra79 <kushagra@dumadu.com>
2025-01-13 21:43:32 +0530

    testing

--- ec2.tf
+++ ec2.tf
@@ -13,7 +13,7 @@ resource "aws_security_group" "alb_sg" {
   }
 
   egress {
-    from_port   = 0
+    from_port   = 0   
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
